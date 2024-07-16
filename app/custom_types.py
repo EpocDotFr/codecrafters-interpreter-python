0b3eb00626950c317f2d162c49d8ea1aad7b238d
EpocDotFr <contact@epoc.fr>
2024-07-16 14:33:31 +0200

    2nd stage

---
+++ app/custom_types.py
@@ -0,0 +1,67 @@
+from typing import Optional, Union
+import dataclasses
+import enum
+
+
+class TokenType(enum.StrEnum):
+    # Single-character tokens
+    LEFT_PAREN = enum.auto()
+    RIGHT_PAREN = enum.auto()
+    LEFT_BRACE = enum.auto()
+    RIGHT_BRACE = enum.auto()
+    COMMA = enum.auto()
+    DOT = enum.auto()
+    MINUS = enum.auto()
+    PLUS = enum.auto()
+    SEMICOLON = enum.auto()
+    SLASH = enum.auto()
+    STAR = enum.auto()
+
+    # One or two character tokens
+    BANG = enum.auto()
+    BANG_EQUAL = enum.auto()
+    EQUAL = enum.auto()
+    EQUAL_EQUAL = enum.auto()
+    GREATER = enum.auto()
+    GREATER_EQUAL = enum.auto()
+    LESS = enum.auto()
+    LESS_EQUAL = enum.auto()
+
+    # Literals
+    IDENTIFIER = enum.auto()
+    STRING = enum.auto()
+    NUMBER = enum.auto()
+
+    # Keywords
+    AND = enum.auto()
+    CLASS = enum.auto()
+    ELSE = enum.auto()
+    FALSE = enum.auto()
+    FUN = enum.auto()
+    FOR = enum.auto()
+    IF = enum.auto()
+    NIL = enum.auto()
+    OR = enum.auto()
+    PRINT = enum.auto()
+    RETURN = enum.auto()
+    SUPER = enum.auto()
+    THIS = enum.auto()
+    TRUE = enum.auto()
+    VAR = enum.auto()
+    WHILE = enum.auto()
+
+    EOF = enum.auto()
+
+
+@dataclasses.dataclass
+class Token:
+    type_: TokenType
+    lexeme: str = ''
+    literal: Optional[Union[str, int, float, bool]] = None
+
+    def __str__(self) -> str:
+        return '{} {} {}'.format(
+            self.type_.name,
+            self.lexeme,
+            'null' if self.literal is None else self.literal
+        )
--- app/lexer.py
+++ app/lexer.py
@@ -1,14 +1,38 @@
-from io import StringIO
+from app.custom_types import Token, TokenType
+from typing import List, TextIO
 
 
 class Lexer:
-    f: StringIO
+    f: TextIO
+    tokens: List[Token]
 
-    def __init__(self, f: StringIO):
+    def __init__(self, f: TextIO):
         self.f = f
+        self.tokens = []
 
     def tokenize(self):
-        pass
+        while True:
+            c = self.f.read(1)
+
+            if not c:
+                break
+
+            if c == '(':
+                self.tokens.append(Token(
+                    TokenType.LEFT_PAREN,
+                    c
+                ))
+            elif c == ')':
+                self.tokens.append(Token(
+                    TokenType.RIGHT_PAREN,
+                    c
+                ))
+
+        self.tokens.append(Token(
+            TokenType.EOF
+        ))
 
     def __str__(self) -> str:
-        return 'EOF  null'
+        return '\n'.join([
+            str(t) for t in self.tokens
+        ])
